d800a7a77e21105f7e15bdda5e2bb30e36309984
acmenes <acmenescompositions@gmail.com>
2021-08-22 20:58:20 -0400

    first commit some tests written

---
+++ src/Card.test.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+import Card from "./Card"
+import { exportAllDeclaration } from "@babel/types"
+
+it("renders without crashing", function(){
+    render(<Card />)
+})
+
+it("matches snapshot", function(){
+    const {asFragment} = render(<Card />)
+    expect(asFragment).toMatchSnapshot();
+})
